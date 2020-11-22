cbfc22dead413459f85cae696c28109fa9796ab7
Glitch (discordjsv12basitaltyapi) <none>
2020-11-22 11:52:30 +0000

    🔑⛈ Checkpoint ./komutlar/token.js:21421149/274 ./komutlar/fadness.js:21421149/51

--- komutlar/fadness.js
+++ komutlar/fadness.js
@@ -10,8 +10,9 @@ exports.run = (client, message, params) => {
     return message.author.send(ozelmesajuyari); }
     if (message.channel.type !== 'dm') {
       const embed = new Discord.MessageEmbed()
-    .setAuthor(message.author.username + ' Kanka sana tokenimi vereceğime gerçekten inandın mı \nGerçekten inandıysan helal olsun.')
+    .setAuthor(message.author.username + ' Kanka şu fotoğrafa bak anlarsın:')
     .setColor("RANDOM")
+    .setImage("https://cdn.discordapp.com/attachments/779776472520851490/780035830483648512/kuzi.jpg")
     .setTimestamp()
     return message.channel.send(embed);
     }
@@ -19,11 +20,11 @@ exports.run = (client, message, params) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['fadness'],
   permLevel: 0
 };
 exports.help = {
-  name: 'token',
-  description: 'token',
-  usage: 'token'
+  name: 'fadness',
+  description: 'fadness',
+  usage: 'fadness'
 };
